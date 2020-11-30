79d9ba6082dba68fc8dcb27c023a9436f7bab50b
jongphago <jonghyun.d.kim@gmail.com>
2020-11-30 17:36:24 +0900

    주어진 코드 시작

---
+++ main.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX_VERTICES 30;
+#define MIN2(x, y) ((x) < (y) ? (x) : (y))
+#include <stdio.h>
+#include <stdlib.h>
+
+short int dfn[30]; // 오류
+short int low[30]; // 오류
+
+int num;
+void init();
+void bicon(int, int);
+
+void main()
+{
+	// 파일 오픈하여 파일을 읽어 adjancey list에 저장한다.
+
+	// init();
+	// bicon(3, -1);
+	return;
+}
